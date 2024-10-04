d2644b1b632d25d44c257328d59e49b20b622e76
Debnath Mondal <mondal.deb1610@gmail.com>
2024-10-04 23:49:55 +0530

    calculate wage including part time

--- EmployeeClass.js
+++ EmployeeClass.js
@@ -16,27 +16,45 @@ class Employee {
     }
 
    DailyWages(){
-    let hours=8;
-    let wages=0
-    let wagePerHour=20;
-    if(this.attendance){
-      wages=hours*wagePerHour
-      return wages
+    if(this.attendance==2){
+      let hours=8;
+      let wages=0
+      let wagePerHour=20;
+      if(this.attendance){
+        wages=hours*wagePerHour
+        this.Wages=wages
+        return `Full Time ${wages} $`
+      }
+    }
+    else if(this.attendance==1){
+      let hours=4;
+      let wages=0
+      let wagePerHour=20;
+      if(this.attendance){
+        this.Wages=wages
+        wages=hours*wagePerHour
+        return `Part Time ${wages} $`
+      }
     }
     else{
-      return 0;
+      this.Wages=0
+      return "Absent 0 $"
     }
+    
   }
 
     displayInfo() {
       console.log(`Employee Name: ${this.name}, ID: ${this.id} , Attendance: ${this.EmployeeAttendence()} , Wages: ${this.DailyWages()}`);
     }
-
-  }
+}
   
 function getRandom() {
-    return Math.floor(Math.random() * 2);
+    return Math.floor(Math.random() * 3);
 }
+
+
+
+
 
 
 const emp1 = new Employee("Priyanshu Kumar", 101);
